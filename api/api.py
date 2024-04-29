a3c9b3b4d38080763ebedcc60b19fba29e27e4e3
liam.m.hess <liam.m.hess@gmail.com>
2024-04-29 18:24:04 +0000

    add error handling for duplicated user

--- api/api.py
+++ api/api.py
@@ -1,10 +1,11 @@
 import requests
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 from api.backend_logic import BackendLogic
 from cronjob.cronjob import Cronjob
 import os
+import psycopg
 
 class SubscriptionRequest(BaseModel):
 	email: str
@@ -18,9 +19,12 @@ app = FastAPI()
 backend = BackendLogic(os.environ.get("OPENAI_API_KEY"), os.environ.get("POSTGRES_PASSWORD"))
 
 @app.post("/subscribe", status_code=201)
-async def subscribe(item: SubscriptionRequest):
-    backend.add_user(item.email, item.interests)
-    Cronjob().hackernews_to_mail_flow(daily_flow = False, user_list = [item.email])
+async def subscribe(item: SubscriptionRequest, response: Response):
+    try:
+        backend.add_user(item.email, item.interests)
+        Cronjob().hackernews_to_mail_flow(daily_flow = False, user_list = [item.email])
+    except psycopg.errors.UniqueViolation:
+        response.status_code = status.HTTP_409_CONFLICT
     return None
 
 @app.post("/unsubscribe", status_code=204)
